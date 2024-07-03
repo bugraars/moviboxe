87b2053f97a2cd0498b67ae5f3f3e9248620ddd3
Buğra Arslan <aarslanbugra@outlook.com>
2024-07-03 13:00:34 +0300

    v1.3

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 import type { Metadata } from "next";
-import { DM_Sans } from '@next/font/google';
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/components/navbar/page';
 import Footer from '@/components/footer/page';
